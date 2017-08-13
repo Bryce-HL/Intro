50370c8fc929475a8e80dc51df148273a243b46f
Bryce-HL <lommelb@gmail.com>
2017-08-13 17:45:15 -0400

    background change and image to render added

--- Snake/food_spawn.py
+++ Snake/food_spawn.py
@@ -3,15 +3,26 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
-from panda3d.core import Point3
+from panda3d.core import Point3, LPoint3
 from random import randint
 
+SPRITE_POS = 55 
+
 class MyApp (ShowBase):
+
+    def loadObject(tex=None, pos=LPoint3(0, 0), depth=SPRITE_POS, scale=1,
+               transparency=True):
+
+               self.food = loadObject("square.jpg")
 
     def __init__(self):
         ShowBase.__init__(self)
 
     
+        self.setBackgroundColor((0, 0, 1, 2))
+
+        
+        
     
     def spawn_food(self):       
         self.foodActor = Actor("models/box")
